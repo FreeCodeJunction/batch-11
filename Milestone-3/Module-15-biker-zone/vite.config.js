a268b9fc549fd4aa4242f05ceabffaf3b6523272
FreeCodeJunction <ahnafmottaki2022@gmail.com>
2025-02-02 10:40:02 +0600

    refurvished

--- Milestone-3/Module-15-biker-zone/vite.config.js
+++ Milestone-3/Module-15-biker-zone/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "batch-11/Milestone-3/Module-15-biker-zone/",
   plugins: [tailwindcss()],
 });
